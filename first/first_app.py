4a718975ebac859e452ffcc7fb0c235938c9da83
Ibrahim Dolek <idolek28@gmail.com>
2026-04-22 16:32:22 +0300

    type conversion

--- first/first_app.py
+++ first/first_app.py
@@ -1,12 +1,12 @@
-def tax_calculate():
-    tax_rate = 0.20
+# def tax_calculate():
+#     tax_rate = 0.20
 
-    product_price = int(input("Urun Fiyatı giriniz:"))
+#     product_price = int(input("Urun Fiyatı giriniz:"))
 
-    total_tax = product_price * tax_rate
-    taxfree_price = product_price - total_tax
+#     total_tax = product_price * tax_rate
+#     taxfree_price = product_price - total_tax
 
-    print("Urunun vergisiz fiyatı:", taxfree_price)
-    print(total_tax, " TL Vergi İadenizi alabilirsiniz.")
+#     print("Urunun vergisiz fiyatı:", taxfree_price)
+#     print(total_tax, " TL Vergi İadenizi alabilirsiniz.")
 
-tax_calculate()
+# tax_calculate()
---
+++ first/type_conversion.py
@@ -0,0 +1,46 @@
+'''
+x = input('1. sayı:')
+y = input('2. sayı:')
+
+print(type(x))
+print(type(y))
+
+sum = int(x) + int(y)
+
+print(sum)
+'''
+
+'''
+x = 5               #int
+y = 2.5             #float
+name = 'Teto'       #string
+is_online = True    #bool
+
+print (type(x))
+print (type(y))
+print (type(name))
+print (type(is_online))
+
+x = float(x)
+print(type(x))
+
+result = x
+print(result)
+'''
+'''
+product_str = "Mechanichal Keyboard"
+price_str = "1250.50"
+piece_str = "2"
+user_balance_str = "3000"
+
+price = float(price_str)
+piece = int(piece_str)
+user_balance = float(user_balance_str)
+
+total_price = price * piece
+
+remaining_balance = user_balance - total_price
+
+print("2 adet Mekanik Klavye satın alındı. Kalan bakiyeniz: ", remaining_balance, " TL")
+
+'''
